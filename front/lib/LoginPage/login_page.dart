013a337d39028198a86b98d4699657aa8f84b838
Rodrigo Perrucci Macharelli <rmacharelli@masterdom.inf.br>
2020-11-28 23:58:07 -0300

    githubpages

--- front/lib/LoginPage/login_page.dart
+++ front/lib/LoginPage/login_page.dart
@@ -58,7 +58,7 @@ class _LoginPageState extends State<LoginPage> {
           padding: EdgeInsets.fromLTRB(
               MediaQuery.of(context).size.width * 0.02, 10, 0, 0),
           child: Text(
-            "Editor Visual",
+            "Editor Visual - 2020",
             style: TextStyle(
                 fontSize: 45, fontWeight: FontWeight.bold, color: Colors.white),
             textAlign: TextAlign.left,
